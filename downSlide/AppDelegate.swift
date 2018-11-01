acd7e9e5d92ce8790765759275364dabdabd0d96
Jamis Charles <jacharles@paypal.com>
2018-10-31 20:35:09 -0700

    Initial Commit

---
+++ downSlide/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  downSlide
+//
+//  Created by Charles, Jamis on 10/31/18.
+//  Copyright © 2018 Charles, Jamis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ downSlide/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  downSlide
+//
+//  Created by Charles, Jamis on 10/31/18.
+//  Copyright © 2018 Charles, Jamis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
