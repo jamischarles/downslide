998d5e1388018a4bb1f9fbf40b2853b3ea892ef1
Jamis Charles <jacharles@paypal.com>
2018-11-05 23:13:04 -0800

    Able to parse rgb, hex bgcolor from file

--- downSlide/MarkdownToSlideParser.swift
+++ downSlide/MarkdownToSlideParser.swift
@@ -12,15 +12,25 @@
 import Foundation
 import Cocoa // for NSView
 
+// config for the whole file
+struct GlobalConfig {
+    var bgColor: NSColor // theme bgcolor
+}
+
 // returns array of views
+// FIXME: If there's any state that the slide needs to hold onto, then we should really use "Slide" objects from "Slide" class
 func getSlidesFromContentString(rawString: String) -> [NSView] {
     
     let stringsBySlide = splitFilecontentIntoStringsBySlide(rawString)
     
+    // extract global config for the document (theme, styles, etc)
+    // FIXME: Must the config be at the top?
+    let config = extractGlobalConfig(stringsBySlide[0])
+    
     
     var slides: [NSView] = []
     for string in stringsBySlide {
-        slides.append(parseStringToSlideView(string))
+        slides.append(parseStringToSlideView(string, globalConfig: config))
     }
     
     return slides
@@ -38,8 +48,8 @@ private func splitFilecontentIntoStringsBySlide(_ rawFileString: String) -> [Str
 
 // this is where the main magic is going to happen
 // turn a slide string into views that can be rendered
-private func parseStringToSlideView(_ str: String) -> NSView {
-    print("###########do something#############")
+private func parseStringToSlideView(_ str: String, globalConfig: GlobalConfig) -> NSView {
+    //print("###########do something#############")
     //let view = NSView();
     
     
@@ -54,7 +64,7 @@ private func parseStringToSlideView(_ str: String) -> NSView {
     
     let title = str.components(separatedBy: "\n")[0] // just take first line for now...
     
-    return makeFormattedView(title: title)
+    return makeFormattedView(title: title, bgColor: globalConfig.bgColor)
     
     let textField = NSTextField(string: str)
     
@@ -70,8 +80,9 @@ private func parseStringToSlideView(_ str: String) -> NSView {
 
 // TODO: consider putting this somewhere else?
 // TODO: move this to subClass or model, or slides.swift or separate class file or something...
-func makeFormattedView(title: String) -> NSView{
-    
+
+// TODO: add default bg color...
+func makeFormattedView(title: String, bgColor: NSColor) -> NSView {
     
     //rgb: 106, 215, 152
     let green = NSColor(red:106/255, green:215/255, blue:152/255, alpha:1.000)
@@ -201,7 +212,9 @@ func makeFormattedView(title: String) -> NSView{
     // add bg color
     // Q: Use view instead?
     stackView.wantsLayer = true
-    stackView.layer?.backgroundColor = blue.cgColor
+    // set slide bg color
+    stackView.layer?.backgroundColor = bgColor.cgColor
+    // stackView.layer?.backgroundColor = blue.cgColor
     
     
     // FIXME: Do we need to apply all the constraints and all these transformations when we show it only?
@@ -225,4 +238,119 @@ func makeFormattedView(title: String) -> NSView{
     
     return stackView
     
+}
+
+// converts a config string (from markdown) into a config object...
+// FIXME: is there a better data structure I can use?
+func extractGlobalConfig(_ rawStr: String) -> GlobalConfig {
+    
+    // TODO: see if we can cut off the parts after closing ```
+    // keep only the codeFence part
+    let configStr = rawStr.components(separatedBy: "```")[1]
+    
+    
+    // TODO: search for hex vs rgb
+    // doubleslach to escape ( and ). Weird...
+    
+    // FIXME: make this match better
+    let range = NSRange(location: 0, length: configStr.utf16.count)
+    let regex = try! NSRegularExpression(pattern: "background-color: rgb")
+    let isRgb = regex.firstMatch(in: configStr, options: [], range: range) != nil
+    
+    
+    
+    if isRgb {
+        let rgbGroups = configStr.capturedGroups(withRegex: "background-color: rgb\\((.*)\\);?")
+        let config = GlobalConfig(bgColor: rgbStringToNSColor(rgbStr: rgbGroups[0]))
+        print("Choose RGB", config)
+        return config
+        
+    } else {
+        let hexGroups = configStr.capturedGroups(withRegex: "background-color: #(.*);?")
+        let config = GlobalConfig(bgColor: hexStringToNSColor(hex: hexGroups[0]))
+        print("Choose HEX", config)
+        return config
+    }
+    
+}
+
+// "1,22,39" -> NSColor
+func rgbStringToNSColor(rgbStr: String) -> NSColor {
+    let values = rgbStr.split(separator: ",")
+    
+    // parse from string to int
+    let red = Int(String(values[0]))
+    let green = Int(String(values[1]))
+    let blue = Int(String(values[2]))
+    
+    //print("rgb: \(red),\(green),\(blue)")
+    
+    // then parse to CGFloat
+    return NSColor(red: CGFloat(red!)/255, green: CGFloat(green!)/255, blue: CGFloat(blue!)/255, alpha:1.000)
+    //return NSColor(red: values[0]/255, green: values[1]/255, blue: values[2]/255, alpha:1.000)
+    //return NSColor(red: Int(values[0])/255, green: Int(values[1])/255, blue: Int(values[2])/255, alpha:1.000)
+}
+
+// UTILS. FIXME: consider moving this out into something else?
+// Stolen from https://stackoverflow.com/questions/24263007/how-to-use-hex-colour-values
+func hexStringToNSColor (hex:String) -> NSColor {
+    var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+    
+    if (cString.hasPrefix("#")) {
+        cString.remove(at: cString.startIndex)
+    }
+    
+    // Deal with 3 character Hex strings
+    if hex.count == 3 {
+        let redHex   = cString.first!
+        let greenHex = cString[cString.index(after: cString.startIndex)] // FIXME: is this really the best way to get the 2nd value?!?!?!
+        let blueHex  = cString.last!
+        
+        cString = "\(redHex)\(redHex)\(greenHex)\(greenHex)\(blueHex)\(blueHex)"
+    }
+    
+    if ((cString.count) != 6) {
+        return NSColor.gray
+    }
+    
+    var rgbValue:UInt32 = 0
+    Scanner(string: cString).scanHexInt32(&rgbValue)
+    
+    return NSColor(
+        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+        alpha: CGFloat(1.0)
+    )
+}
+
+
+// extend String with regex capture groups
+// https://samwize.com/2016/07/21/how-to-capture-multiple-groups-in-a-regex-with-swift/
+extension String {
+    func capturedGroups(withRegex pattern: String) -> [String] {
+        var results = [String]()
+        
+        var regex: NSRegularExpression
+        do {
+            regex = try NSRegularExpression(pattern: pattern, options: [])
+        } catch {
+            return results
+        }
+        
+        let matches = regex.matches(in: self, options: [], range: NSRange(location:0, length: self.count))
+        
+        guard let match = matches.first else { return results }
+        
+        let lastRangeIndex = match.numberOfRanges - 1
+        guard lastRangeIndex >= 1 else { return results }
+        
+        for i in 1...lastRangeIndex {
+            let capturedGroupIndex = match.range(at: i)
+            let matchedString = (self as NSString).substring(with: capturedGroupIndex)
+            results.append(matchedString)
+        }
+        
+        return results
+    }
 }
--- testing.md
+++ testing.md
@@ -1,3 +1,13 @@
+```config:theme
+// Should this be just a prop? Or is there value in this actually being css? compare and try both...
+
+.background-slide {
+    
+    background-color: #333
+    background-color: rgb(1,22,39);
+}
+```
+
 # Speaking to baby turtles
 The best
 ---
